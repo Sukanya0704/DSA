6bc4b960b512aab8b6bd649e0516078d71d3bf26
Sukanya <sukanyagupta0730@gmail.com>
2026-01-12 18:47:58 +0530

    Minimum Time Visiting All Points

---
+++ maths/min_time_visiting_all_nodes.cpp
@@ -0,0 +1,18 @@
+// class Solution {
+// public:
+//     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+//         int totalTime = 0;
+        
+//         for (int i = 0; i < points.size() - 1; i++) {
+//             int x1 = points[i][0], y1 = points[i][1];
+//             int x2 = points[i+1][0], y2 = points[i+1][1];
+
+//             int dx = abs(x2 - x1);
+//             int dy = abs(y2 - y1);
+            
+//             totalTime += max(dx, dy);
+//         }
+        
+//         return totalTime;
+//     }
+// };
