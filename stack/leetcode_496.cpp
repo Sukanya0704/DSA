b7cd915a2f0433e7eeba2d81a8a0dd612fc31677
Sukanya <sukanyagupta0730@gmail.com>
2026-07-03 12:53:22 +0530

    Next Greater Element I

---
+++ stack/leetcode_496.cpp
@@ -0,0 +1,21 @@
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         int n = nums2.size();
+//         unordered_map<int, int> nge;
+
+//         stack<int> s;
+//         for(int i=n-1; i>=0; i--){
+//             while(!s.empty() && s.top() <= nums2[i]) s.pop();
+//             if(s.empty()) nge[nums2[i]] = -1;
+//             else nge[nums2[i]] = s.top();
+//             s.push(nums2[i]);
+//         }
+
+//         vector<int> ans;
+//         for(int i=0; i<nums1.size(); i++){
+//             ans.push_back(nge[nums1[i]]);
+//         }
+//         return ans;
+//     }
+// };
