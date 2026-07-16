38aa3847c7d67af2c83a506c09e9da5b0c598cab
Sukanya <sukanyagupta0730@gmail.com>
2026-07-16 12:57:36 +0530

    Minimum Path Sum

---
+++ dp/leetcode_64.cpp
@@ -0,0 +1,23 @@
+// class Solution {
+// public:
+//     int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
+//         if(i == 0 && j == 0) return grid[i][j];
+
+//         if(i < 0 || j < 0) return 1e9;
+
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         int up = solve(i-1, j, grid, dp) + grid[i][j];
+//         int left = solve(i, j-1, grid, dp) + grid[i][j];
+
+//         return dp[i][j] = min(up, left);
+//     }
+
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<vector<int>> dp(m, vector<int>(n, -1));
+//         //Similarlly we can perform tabulation and space optimization as well!
+//         return solve(m-1, n-1, grid, dp);
+//     }
+// };
