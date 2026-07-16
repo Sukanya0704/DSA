afff0727f063d4afcfedee490f445701e2c71e7f
Sukanya <sukanyagupta0730@gmail.com>
2026-07-16 12:23:43 +0530

    Unique Paths II

---
+++ dp/leetcode_63.cpp
@@ -0,0 +1,45 @@
+// class Solution {
+// public:
+//     // int solve(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
+//     //     if(i == 0 && j== 0 && obstacleGrid[i][j] == 0) return 1;
+
+//     //     if(i < 0 || j < 0) return 0;
+
+//     //     if(dp[i][j] != -1) return dp[i][j];
+
+//     //     if(obstacleGrid[i][j] == 1) return 0;
+
+//     //     int up = solve(i-1, j, obstacleGrid, dp);
+//     //     int left = solve(i, j-1, obstacleGrid, dp);
+
+//     //     return dp[i][j] = up + left;
+//     // }
+
+//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+//         int m = obstacleGrid.size();
+//         int n = obstacleGrid[0].size();
+
+//         if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+
+//         vector<vector<int>> dp(m, vector<int>(n, 0));
+//         dp[0][0] = 1;
+
+//         for(int i=0; i<m; i++){
+//             for(int j=0; j<n; j++){
+//                 if(obstacleGrid[i][j] == 1) continue;
+//                 if(i == 0 && j == 0) continue;
+
+//                 int up = 0;
+//                 int left = 0;
+//                 if(i > 0) up = dp[i-1][j];
+//                 if(j > 0) left = dp[i][j-1];
+
+//                 dp[i][j] = up + left;
+//             }
+//         }
+//         //Space Optimization can be done using prev and cur vectors
+
+//         return dp[m-1][n-1];
+//         //return solve(m-1, n-1, obstacleGrid, dp);
+//     }
+// };
