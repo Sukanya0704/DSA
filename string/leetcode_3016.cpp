31ba25ca7ec664c6531976e17ddcb836ba654905
Sukanya <sukanyagupta0730@gmail.com>
2026-07-31 09:37:49 +0530

    Minimum Number of Pushes to Type Word II

---
+++ string/leetcode_3016.cpp
@@ -0,0 +1,31 @@
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         vector<int> freq(26, 0);
+//         for(auto ch : word){
+//             freq[ch -'a']++;
+//         }
+
+//         sort(freq.rbegin(), freq.rend()); 
+//         int ans = 0;
+
+//         for(int i=0; i<26; i++){
+//             if(freq[i] == 0) break;
+
+//             if(i < 8){
+//                 ans += freq[i];
+//             }
+//             else if(i < 16){
+//                 ans += 2*freq[i];
+//             }
+//             else if(i < 24){
+//                 ans += 3*freq[i];
+//             }
+//             else{
+//                 ans += 4*freq[i];
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
